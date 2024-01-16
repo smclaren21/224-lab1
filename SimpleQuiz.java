9b6b881782885f586d45e92c9e3fac604065b7da
Kesler <superkes12345@gmail.com>
2024-01-16 13:17:56 -0800

    Added Quiz File

---
+++ SimpleQuiz.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SimpleQuiz {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Welcome to our quiz! \n Please answer questions by answering the number corresponding with your preferred response:");
+	}
+}
