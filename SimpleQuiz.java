ef6b4c5685ced8d5be4d30a9f43cea269df166ec
Sam McLaren <sammymac211517@gmail.com>
2024-01-18 12:38:22 -0800

    freshened up code and made it work

--- SimpleQuiz.java
+++ SimpleQuiz.java
@@ -5,7 +5,7 @@ public class SimpleQuiz {
 		Scanner in = new Scanner(System.in);
 		int score = 0;
 
-		System.out.println("Welcome to our quiz! \n Please answer questions by answering the number corresponding with your preferred response:");
+		System.out.println("Welcome to our quiz! \nPlease answer questions by answering the letter corresponding with your preferred response:");
 		System.out.println("Please enter your name to begin:");
 		String name = in.nextLine();
 		//Question 1
@@ -13,8 +13,8 @@ public class SimpleQuiz {
 		System.out.println("A. Iceland		B. Madagascar");
 		System.out.println("C. New Zealand	D. Russia");
 		
-		char answer1 = in.nextChar();
-		if (answer1 == 'B' || answer1 == 'b') {
+		String answer1 = in.nextLine();
+		if (answer1.toUpperCase().equals("B")) {
 			score++;
 			System.out.println("Correct!");
 		} else {
